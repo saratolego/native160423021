54b40b9e53582d216026bfbcaebb4d72f637fbe3
Sar-ato-lego <s160423021@student.ubaya.ac.id>
2025-10-07 17:35:42 +0700

    feat: Initial commit
    Initializes a new Android Studio project named "SatuKelompok_ANMP_ProjectUTS".
    
    This commit includes the basic project structure, Gradle configuration files, and default application resources. It sets up the main activity and three empty fragments: `FragmentData`, `FragmentProfilAnak`, and `FragmentUkur`, along with their corresponding layout files.
    
    Key additions:
    -   Root and app-level `build.gradle.kts` files with initial dependencies for AndroidX, Material Design, ConstraintLayout, Lifecycle, and Navigation.
    -   `MainActivity` and basic layout.
    -   `FragmentData`, `FragmentProfilAnak`, and `FragmentUkur` classes and layouts.
    -   Standard `.gitignore` files for Android projects.
    -   Application manifest, themes, colors, and string resources.
    -   Default launcher icons and XML drawables.
    -   Gradle wrapper and project-wide properties.

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.saradev.satukelompok_anmp_projectuts"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.saradev.satukelompok_anmp_projectuts"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+
+}
